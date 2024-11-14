ad9c74b77b94c3dd9e3ee37608441f2e32e1d91f
RiXXXhun <100133611+RiXXXhun@users.noreply.github.com>
2024-11-14 15:01:24 +0100

    FSD

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,7 +7,7 @@ export const AppDataSource = new DataSource({
     type: "mysql",
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT as string),
-    username: process.env.DB_USERNAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     synchronize: true,
